15d8893c164662130b77d898acf27f38c1badf9a
Justin Jones <hock3ydud3@gmail.com>
2023-01-15 18:36:03 -0600

    basic types

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log("hello world");
+const isOpen: boolean = false;
+// this fails because name is defined on the window object already
+// const name: string = 'Scott';
+const theName: string = 'Scott';
+
+const anAge: number = 37;
+const alsoAge: number = 35.06;
+
+const list: number[] = [0, 1, 2];
+
+// tuple
+const me: [string, number, boolean] = [theName, anAge, false];
+
+//enum
+enum Job {
+  WebDev,
+  WebDesigner,
+  Engineer,
+}
+const job: Job = Job.Engineer;
+
+const phone: any = 'iPhone';
+const tablet: any = 3;
+
+// never -> ok i dunno...can't assign to it? none?
+// const something: never = 3;
